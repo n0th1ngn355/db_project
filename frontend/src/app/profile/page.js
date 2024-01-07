5eb492e21ee210239e1de090b5912a3280518f02
Melugsam <melugsam@mail.ru>
2024-01-07 11:40:33 +0300

    myprofile v1.1

--- frontend/src/app/profile/page.js
+++ frontend/src/app/profile/page.js
@@ -2,27 +2,38 @@
 import React from 'react';
 import Sidebar from '@/app/components/Sidebar/sidebar';
 import 'bootstrap/dist/css/bootstrap.css'
+import './profile.css'
 
 const MainLayout = () => {
+  const descriptionStyle = {
+    marginLeft: '5px',
+    marginRight: '30px'
+  };
+
   return (
     <div className="container mt-5">
       <div className='row'>
         <Sidebar info="profile" />
         <div className='col mx-3'>
           <div>
-            <h2 class="mt-5">Ваш профиль</h2>
-            <div class="row mt-4">
+            <div class="row mt-4 profile-info">
               <div className="avatar col-2">
-                <img src="/avatar.svg" alt="Avatar" style={{ width: '100%', maxWidth: '150px', borderRadius: '10%' }} />
+                <img src="/avatar.svg" alt="Avatar"/>
               </div>
-              <div className="profile-info col">
-                <p>Имя: Ваше имя</p>
-                <p>Подписки: 100</p>
-                <p>Подписчики: 200</p>
-                <p>Описание профиля: Краткое описание вашего профиля</p>
+              <div className="col">
+                <h2 class="name_block">Baboon Limbov</h2>
+                <div className="d-flex">
+                  <label className="num_follow">200</label>
+                  <div class="description_follow" style={descriptionStyle}>Подписчики</div>
+                  <label className="num_follow">100</label>
+                  <div className="description_follow" style={descriptionStyle}>Подписки</div>
+                </div>
+                <div className='mt-2 col description'>
+                Consectetur duis velit minim eiusmod consectetur officia nisi consectetur aliqua elit mollit fugiat reprehenderit mollit ipsum qui. Sint fugiat tempor ipsum reprehenderit cupidatat ex veniam eu eu id aliqua tempor proident. Mollit fugiat voluptate quis laborum sunt non sint qui elit sint.Cillum ipsum consectetur ut et dolor voluptate enim aliqua labore. Ma
+                </div>
               </div>
             </div>
-
+            <hr className='my-4'/>
           </div>
         </div>
       </div>
