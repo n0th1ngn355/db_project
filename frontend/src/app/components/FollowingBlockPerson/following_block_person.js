245cabfc8a45c5d20ab6896eb664b6cad19f1712
TIMA <timofey.fomin@gmail.com>
2024-01-07 19:33:01 +0300

    follows

--- frontend/src/app/components/FollowingBlockPerson/following_block_person.js
+++ frontend/src/app/components/FollowingBlockPerson/following_block_person.js
@@ -2,17 +2,20 @@
 import React from 'react';
 import './following_block_person.css';
 import Image from "next/image";
+import Link from "next/link";
 
 const FollowingBlockPerson = ({ name }) => {
     return (
-        <div className="following-block-person my-2">
-            <div className="avatar-wrapper">
-                <Image src="/avatar.svg" alt="Avatar" width="20" height="20" />
+        <Link className="following-block-person-link" href={`/profile/${name}`} passHref={true}>
+            <div className="following-block-person my-2">
+                <div className="avatar-wrapper">
+                    <Image src="/avatar.svg" alt="Avatar" width="20" height="20"/>
+                </div>
+                <div className="name-wrapper">
+                    <p className="name-text">{name}</p>
+                </div>
             </div>
-            <div className="name-wrapper">
-                <p className="name-text">{name}</p>
-            </div>
-        </div>
+        </Link>
     );
 };
 
