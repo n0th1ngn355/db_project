3fd1551d13b7ec7211a6fa486622d1f41d3221e6
Melugsam <melugsam@mail.ru>
2024-01-08 23:48:41 +0300

    feed ready

--- frontend/src/app/feed/page.js
+++ frontend/src/app/feed/page.js
@@ -10,7 +10,7 @@ import Post from '../components/Post/Post';
 import 'bootstrap/dist/css/bootstrap.css'
 import './feed.css'
 import FeedPostsTypeButton from "@/app/components/FeedPostsTypeButton/FeedPostsTypeButton";
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 const getAuthToken = () => {
   // Вернуть токен из куки или реализовать логику, которая подходит в вашем случае
@@ -19,45 +19,80 @@ const getAuthToken = () => {
 
 const Feed = () => {
 
+  const [postsData, setPostsData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     const authToken = getAuthToken(); // Замените эту функцию на ваш способ получения токена из куки
 
     // Если токен отсутствует, перенаправляем пользователя на страницу логина
     if (!authToken) {
-        window.location.href = '/login';
+      window.location.href = '/login';
+    } else {
+      // Запрос на получение данных о профиле
+      fetch('http://localhost:8000/app/posts/', {
+        method: 'GET',
+        headers: {
+          'Authorization': `Token ${authToken}`,
+          'Content-Type': 'application/json',
+        },
+      })
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error(`Ошибка сети: ${response.status} ${response.statusText}`);
+          }
+        })
+        .then(data => {
+          setPostsData(data);
+          setLoading(false);
+        })
+        .catch(error => {
+          console.error('Произошла ошибка при получении данных профиля:', error.message);
+        });
     }
-}, []);
+  }, []);
 
   return (
     <div className="container mt-5">
       <div className='row h-100'>
-        <Sidebar info="feed"/>
+        <Sidebar info="feed" />
         <div className='col-6 mt-3'>
-            <FeedSearch className='row'/> 
-            <div className='row-1 my-3 searchPost-buttons'>
-                <FeedPostsTypeButton text="Отслеживаемые" pressed={true} />
-                <FeedPostsTypeButton text="Популярные" pressed={false} />
-            </div>
-            <div className='feed'>
-                <Post name="Джером Кук" text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquam eum fuga in, nemo numquam quisquam ullam veniam vitae voluptate. Deleniti ducimus fugit hic mollitia officiis optio possimus temporibus, ullam!" postDayOrTime="12:00" likeAmount="54"></Post>
-                <Post name="Джером Кук" text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquam eum fuga in, nemo numquam quisquam ullam veniam vitae voluptate. Deleniti ducimus fugit hic mollitia officiis optio possimus temporibus, ullam!" postDayOrTime="12:00" likeAmount="54"></Post>
-                <Post name="Джером Кук" text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquam eum fuga in, nemo numquam quisquam ullam veniam vitae voluptate. Deleniti ducimus fugit hic mollitia officiis optio possimus temporibus, ullam!" postDayOrTime="12:00" likeAmount="54"></Post>
-                <Post name="Джером Кук" text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquam eum fuga in, nemo numquam quisquam ullam veniam vitae voluptate. Deleniti ducimus fugit hic mollitia officiis optio possimus temporibus, ullam!" postDayOrTime="12:00" likeAmount="54"></Post>
-                
-            </div>
+          <FeedSearch className='row' />
+          <div className='row-1 my-3 searchPost-buttons'>
+            <FeedPostsTypeButton text="Отслеживаемые" pressed={true} />
+            <FeedPostsTypeButton text="Популярные" pressed={false} />
+          </div>
+          <div className='feed'>
+            {loading ? (
+              <p>Loading...</p>
+            ) : (
+              postsData.map((post, index) => (
+                <Post
+                  key={index}
+                  name={`${post.title} (id Автора: ${post.posted})`}
+                  text={post.content}
+                  postDayOrTime={post.created_at}
+                  initialLiked={post.initialLiked}
+                  likeAmount={post.liked}
+                />
+              ))
+            )}
+          </div>
         </div>
         <div className='col mt-3'>
-            <CreatePostButton text='Создать запись' className='row-1' />
-            <div className="row-1 my-3 recommendations-title-wrapper">
-                <p className='recommendations-title'>Рекомендации</p>
+          <CreatePostButton text='Создать запись' className='row-1' />
+          <div className="row-1 my-3 recommendations-title-wrapper">
+            <p className='recommendations-title'>Рекомендации</p>
+          </div>
+          <div className='row-1'>
+            <div className='row mx-auto'>
+              <Recomenendations name="Биба Бобов" text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi eum explicabo illum quibusdam quidem voluptatum!" likeAmount="67"></Recomenendations>
+              <Recomenendations name="Володя Кислов" text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquam assumenda cum cumque cupiditate deleniti dolorem eligendi et libero modi mollitia officia, placeat quam qui quidem quod sed vel voluptate!" likeAmount="109"></Recomenendations>
+              <Recomenendations name="Акакий Бобов" text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquam eum fuga in, nemo numquam quisquam ullam  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquam eum fuga in, nemo numquam quisquam ullam Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque aut fuga ipsa possimus quia sit veniam? Ab, cumque in minus nesciunt numquam officiis porro quasi. Ipsa nesciunt porro voluptas! Perspiciatis." likeAmount="209"></Recomenendations>
             </div>
-            <div className='row-1'>
-                <div className='row mx-auto'>
-                    <Recomenendations name="Биба Бобов" text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi eum explicabo illum quibusdam quidem voluptatum!" likeAmount="67"></Recomenendations>
-                    <Recomenendations name="Володя Кислов" text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquam assumenda cum cumque cupiditate deleniti dolorem eligendi et libero modi mollitia officia, placeat quam qui quidem quod sed vel voluptate!" likeAmount="109"></Recomenendations>
-                    <Recomenendations name="Акакий Бобов" text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquam eum fuga in, nemo numquam quisquam ullam  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquam eum fuga in, nemo numquam quisquam ullam Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque aut fuga ipsa possimus quia sit veniam? Ab, cumque in minus nesciunt numquam officiis porro quasi. Ipsa nesciunt porro voluptas! Perspiciatis." likeAmount="209"></Recomenendations>
-                </div>
-            </div>
+          </div>
 
 
         </div>
